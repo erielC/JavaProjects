381a54eb2bf86f94c2184114a60817db23d31081
Eriel Cabrera <erielcabrera1234@gmail.com>
2023-03-09 18:37:17 -0500

    completed

--- Columnar Transposition Cipher/crypto.java
+++ Columnar Transposition Cipher/crypto.java
@@ -1,52 +1,39 @@
 import java.util.Scanner;
 
 public class crypto {
-  public static void main(String[] args) {
-    Scanner scan = new Scanner(System.in);
-    String sentence = scan.nextLine();
-    scan.close();
 
-    // Length of the array 
-    int x = (int)(Math.sqrt(sentence.length())) + 1;
-
-    char[][] array = new char[x][x];      
-    // i is rows
-    // j is columns
-
-
-    //PRINTS FOWARD
-    /*int order = 0;
+    public static String sentence (String sentence){
+      int x = (int)(Math.sqrt(sentence.length())) + 1;
+      char[][] array = new char[x][x];   
+      String complete ="";
+      int order = 0;
 
       for (int i = 0; i < x; i++){
         for (int j = 0; j < x; j++){
 
           if(order <= (sentence.length()-1)){
             array[i][j] = sentence.charAt(order);
-            System.out.println(array[i][j]);
             order++;
           }
-        
         }
       }
-      */
 
-      //PRINTS BACKWARD
-      int order = sentence.length();
-
-      for (int i = x; 0 < i; i--){
-        for (int j = 0; j < x; j++){
-
-          if(order > 0){
-            array[i-1][j] = sentence.charAt(order-1);
-            System.out.println(array[i-1][j]);
-            order--;
-          }
-       
+      for (int j = 0; j < x; j++){
+        for (int i = x-1; i>=0; i--){
+          complete += array[i][j];
         }
       }
-  
 
+      return complete;
     }
-  }
 
-  // hellohellohellohello
+
+
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+    String sentence = scan.nextLine();
+    scan.close();
+    
+    System.out.print(sentence(sentence));
+}
+}
