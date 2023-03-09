f67b9a627680454d6ac5afcd93f4a5d54fa5e0dd
Eriel Cabrera <erielcabrera1234@gmail.com>
2023-03-09 07:05:32 -0500

    cyrpto

--- Columnar Transposition Cipher/crypto.java
+++ Columnar Transposition Cipher/crypto.java
@@ -9,13 +9,12 @@ public class crypto {
     // Length of the array 
     int x = (int)(Math.sqrt(sentence.length())) + 1;
 
-
     String[][] array = new String[x][x];
-    for (int r = x; r < x; r--)
-      for (int c = x; 0 < x; c++) {
-        array[r][c]=sentence.substring(0,x+1);
-          System.out.println(array[r][c]);
+    for (int i=x; i>=0; i--){
+      // for (int j=x; 5 > j; j++){
+        System.out.println(i);
+
       }
     }
+  }
 
-}
